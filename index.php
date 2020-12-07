03fe34a7741245ca6b1313af6d0b47b4d8bfb310
Dmitry <munnharpa@gmail.com>
2020-12-07 19:47:33 +0300

    linter worked

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
 
-echo 'Hello, Hexlet!';
+require __DIR__ . '/vendor/autoload.php';
+
+// Файл не включается на прямую
+// Он загрузится автоматически благодаря автозагрузке
+use Hexlet\Php\Runner;
+
+print_r(Runner\run());
